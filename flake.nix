94d1ed3a2d16a6d18c4816b4c07b18f389b4d98e
Daniel Fahey <daniel@knowthydata.co.uk>
2023-09-29 13:33:24 +0100

    simplify

--- flake.nix
+++ flake.nix
@@ -1,35 +1,16 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.disko.url = "github:nix-community/disko";
-  inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
+  description = "OVHcloud dedicated server"
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+
+  };
 
   outputs = { nixpkgs, disko, ... }:
     {
-      nixosConfigurations.hetzner-cloud = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          ./configuration.nix
-        ];
-      };
       nixosConfigurations.ovhcloud = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          ./configuration.nix
-        ];
-      };
-      # tested with 2GB/2CPU droplet, 1GB droplets do not have enough RAM for kexec
-      nixosConfigurations.digitalocean = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          { disko.devices.disk.disk1.device = "/dev/vda"; }
-          ./configuration.nix
-        ];
-      };
-      nixosConfigurations.hetzner-cloud-aarch64 = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
         modules = [
           disko.nixosModules.disko
           ./configuration.nix
